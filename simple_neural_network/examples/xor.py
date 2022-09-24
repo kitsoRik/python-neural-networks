af4c312f826cd8b9e11b57e2f28f390caeb0c44f
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2022-09-24 16:42:22 +0300

    Add more output to `xor` example

--- simple_neural_network/examples/xor.py
+++ simple_neural_network/examples/xor.py
@@ -1,6 +1,11 @@
 from simple_neural_network.lib.neural_network import NeuralNetwork
 from random import shuffle
 
+
+def activate(value):
+    return [round(x[0]) for x in value]
+
+
 nn = NeuralNetwork(2, 2, 1)
 
 # training data
@@ -16,17 +21,18 @@ print("# Initializing ")
 epochs_amount = 50000
 
 for epoch in range(epochs_amount):
+    # randomize array
     shuffle(training_inputs)
 
     for inputs, expected in training_inputs:
         nn.train(inputs, expected)
 
     # print progress
-    if epoch % 1000 == 0:
+    if epoch % (epochs_amount / 100 * 5) == 0:
         print("# Progress ", epoch, "/", epochs_amount, "|", "{0:0.0f}".format(epoch / epochs_amount * 100), "/ 100%")
 
 # test output for real data
-print(nn.feed_forward([0, 0]))  # should be zero
-print(nn.feed_forward([0, 1]))  # should be one
-print(nn.feed_forward([1, 0]))  # should be one
-print(nn.feed_forward([1, 1]))  # should be zero
+print(nn.feed_forward([0, 0]), "passed" if activate(nn.feed_forward([0, 0])) == [0] else "failed, expected " + str([0]) + ", got " + str(activate(nn.feed_forward([0, 0]))))
+print(nn.feed_forward([0, 1]), "passed" if activate(nn.feed_forward([0, 1])) == [1] else "failed, expected " + str([1]) + ", got " + str(activate(nn.feed_forward([0, 1]))))
+print(nn.feed_forward([1, 0]), "passed" if activate(nn.feed_forward([1, 0])) == [1] else "failed, expected " + str([1]) + ", got " + str(activate(nn.feed_forward([1, 0]))))
+print(nn.feed_forward([1, 1]), "passed" if activate(nn.feed_forward([1, 1])) == [0] else "failed, expected " + str([0]) + ", got " + str(activate(nn.feed_forward([1, 1]))))
